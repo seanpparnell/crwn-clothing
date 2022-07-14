c89529b02b062a456c56d6acb51955fb7cb5222c
Sean Patrick Parnell <seanpatrickparnell@gmail.com>
2022-07-14 09:41:37 -0700

    sign-in user with email and password

---
+++ src/routes/authentication/authentication.component.js
@@ -0,0 +1,20 @@
+// import { 
+//   signInWithGooglePopup, 
+//   createUserDocumentFromAuth,
+// } from '../../utils/firebase/firebase.utils';
+
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+import SignInForm from '../../components/sign-in-form/sign-in-form.component';
+
+
+
+const Authentication = () => {
+  return (
+    <div>
+      <SignInForm />
+      <SignUpForm />
+    </div>
+  )
+}
+
+export default Authentication;
