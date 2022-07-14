d828d2ce90072daf6ad5f18b8ab8b36df89b4913
Sean Patrick Parnell <seanpatrickparnell@gmail.com>
2022-07-14 10:05:40 -0700

    authenication page done

--- src/routes/authentication/authentication.component.js
+++ src/routes/authentication/authentication.component.js
@@ -1,7 +1,5 @@
-// import { 
-//   signInWithGooglePopup, 
-//   createUserDocumentFromAuth,
-// } from '../../utils/firebase/firebase.utils';
+import './authentication.styles.scss';
+
 
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 import SignInForm from '../../components/sign-in-form/sign-in-form.component';
@@ -10,7 +8,7 @@ import SignInForm from '../../components/sign-in-form/sign-in-form.component';
 
 const Authentication = () => {
   return (
-    <div>
+    <div className='authentication-container'>
       <SignInForm />
       <SignUpForm />
     </div>
