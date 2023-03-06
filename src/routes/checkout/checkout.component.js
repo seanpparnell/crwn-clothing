344661c152063cb8d9c2970eafdc6a03b962acf0
Sean Patrick Parnell <seanpatrickparnell@gmail.com>
2023-03-06 15:50:11 -0800

    paymentForm component/styling

--- src/routes/checkout/checkout.component.js
+++ src/routes/checkout/checkout.component.js
@@ -3,6 +3,7 @@ import { useContext } from "react";
 import { useSelector } from "react-redux";
 import { selectCartItems, selectCartTotal } from '../../store/cart/cart.selector'
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import PaymentForm from '../../components/payment-form/payment-form.component';
 
 import "./checkout.styles.scss";
 
@@ -35,6 +36,7 @@ const Checkout = () => {
         return <CheckoutItem key={cartItem.id} cartItem={cartItem} />;
       })}
       <span className="total">Total: ${cartTotal}</span>
+      <PaymentForm />
     </div>
   );
 };
