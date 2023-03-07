99567fa4bc10fd3e89ca8647480005edf289ab85
Sean Patrick Parnell <seanpatrickparnell@gmail.com>
2023-03-07 08:11:09 -0800

    testing create-payment-intent

--- src/components/payment-form/payment-form.component.js
+++ src/components/payment-form/payment-form.component.js
@@ -11,16 +11,26 @@ const PaymentForm = () => {
 
   const paymentHandler = async (e) => {
     e.preventDefault();
+
     if(!stripe || !elements) {
       return;
     }
     
-  }
+    const response = await fetch('/.netlify/functions/create_payment_intent', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ amount: 10000 }),
+    }).then((res) => res.json());
+
+    console.log(response)
+  };
 
   return (
     <>
       <PaymentFormContainer>
-        <FormContainer>
+        <FormContainer onSubmit={paymentHandler}>
           <h2>Credit Card Payment: </h2>
           <CardElement />
           <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
