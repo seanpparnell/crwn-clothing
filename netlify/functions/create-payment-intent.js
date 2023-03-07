6b18af96572ec5873815d66fba34e8c6ad60d0c8
Sean Patrick Parnell <seanpatrickparnell@gmail.com>
2023-03-07 09:43:08 -0800

    stripe bug

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,5 +1,5 @@
-require("dotenv").config();
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event) => {
   try {
@@ -7,19 +7,20 @@ exports.handler = async (event) => {
 
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
-      currency: "inr",
-      payment_method_types: ["card"],
+      currency: 'usd',
+      payment_method_types: ['card'],
     });
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ paymentIntent })
-    }
+      body: JSON.stringify({ paymentIntent }),
+    };
   } catch (error) {
-      console.log({error});
-    
+    console.log({ error });
+
     return {
-      statusCode: 400,
-      body: JSON.stringify({ error })
+      status: 400,
+      body: JSON.stringify({ error }),
     };
   }
 };
